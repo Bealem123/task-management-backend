c015ab3154403f4f04accbcc320066e082536d10
Bealem123 <sintayehubealem411@gmail.com>
2025-07-12 00:26:53 -0700

    Update app.js

--- app.js
+++ app.js
@@ -4,9 +4,16 @@ const cors = require("cors");
 
 const authRoutes = require("./routes/authRoutes");
 const taskRoutes = require("./routes/taskRoutes");
+
 app.use(cors());
 app.use(express.json());
 
+// Root route to respond on GET /
+app.get("/", (req, res) => {
+  res.send("Welcome to Task Manager Backend API");
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api", taskRoutes);
+
 module.exports = app;
